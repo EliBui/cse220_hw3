6bb307990e6dcc1bbcaa107a09eb6c0e6b22b0df
EliBui <91813317+EliBui@users.noreply.github.com>
2024-03-22 03:12:42 +0000

    place_tiles

--- src/hw3.c
+++ src/hw3.c
@@ -4,20 +4,24 @@
 #include <ctype.h>
 #include <assert.h>
 #include <stdbool.h>
-#include "hw3.h" 
+#include "hw3.h"
 
-#define DEBUG(...) fprintf(stderr, "[          ] [ DEBUG ] "); fprintf(stderr, __VA_ARGS__); fprintf(stderr, " -- %s()\n", __func__)
+#define DEBUG(...)                              \
+    fprintf(stderr, "[          ] [ DEBUG ] "); \
+    fprintf(stderr, __VA_ARGS__);               \
+    fprintf(stderr, " -- %s()\n", __func__)
 
 bool firstWord = true;
 
-GameState* initialize_game_state(const char *filename) {
+GameState *initialize_game_state(const char *filename) {
+    (void)filename;
     FILE *file = fopen(filename, "r");
     char c;
     char numOfRow = 0;
     char numOfCol = 0;
-    while(fscanf(file, "%c", &c) == 1) {
-        if(c != '\n') {
-            if(numOfRow == 0) {
+    while (fscanf(file, "%c", &c) == 1) {
+        if (c != '\n') {
+            if (numOfRow == 0) {
                 numOfCol++;
             }
         } else {
@@ -26,55 +30,71 @@ GameState* initialize_game_state(const char *filename) {
     }
 
     char **board = (char **)malloc(numOfRow * sizeof(char *));
-    for(int i = 0; i < numOfRow; i++) {
+    for (int i = 0; i < numOfRow; i++) {
         board[i] = (char *)malloc(numOfCol * sizeof(char));
     }
 
     int **boardDepth = (int **)malloc(numOfRow * sizeof(int *));
-    for(int i = 0; i < numOfRow; i++) {
+    for (int i = 0; i < numOfRow; i++) {
         boardDepth[i] = (int *)malloc(numOfCol * sizeof(int));
     }
 
-    rewind(file); //move pointer of file back to beginning
+    rewind(file); // move pointer of file back to beginning
 
-    for(int i = 0; i < numOfRow; i++) {
-        for(int j = 0; j < numOfCol; j++) {
+    for (int i = 0; i < numOfRow; i++) {
+        for (int j = 0; j < numOfCol; j++) {
             fscanf(file, "%c", &c);
-            if(c != '\n') {
+            if (c != '\n') {
                 board[i][j] = c;
                 boardDepth[i][j] = (c == '.') ? 0 : 1;
-                if(c != '.') {
+                if (c != '.') {
                     firstWord = false;
                 }
             }
         }
     }
 
-    struct GameState *game = (struct GameState*)malloc(sizeof(struct GameState));
+    GameState *game = (GameState *)malloc(sizeof(GameState));
     game->numOfRow = numOfRow;
     game->numOfCol = numOfCol;
     game->board = board;
+    game->boardDepth = boardDepth;
     game->previous = NULL;
 
     return game;
 }
 
 bool checkValid(GameState *game, int row, int col, char direction, const char *tiles) {
-    if(row < 0 || col < 0 || row > game->numOfRow || col > game->numOfCol) {
+    (void)game;
+    (void)row;
+    (void)col;
+    (void)direction;
+    (void)tiles;
+
+    if (row < 0 || col < 0 || row > game->numOfRow || col > game->numOfCol) {
         return false;
-    } else if(direction != 'H' && direction != 'V') {
+    } else if (direction != 'H' && direction != 'V') {
         return false;
     }
 
     char **board = game->board;
     int **boardDepth = game->boardDepth;
 
-    char fullWordCreated[46];//45 is the length of the longest word in the english dictionary
+    char fullWordCreated[46]; // 45 is the length of the longest word in the english dictionary
     int index = 0;
 
     int r, c;
-    bool stuffBefore = false, spaceInTiles = false, stuffAfter = false;//for checking if covers up the entire previous word
-    
+    bool stuffBefore = false, spaceInTiles = false, stuffAfter = false; // for checking if covers up the entire previous word
+
+    (void)board;
+    (void)boardDepth;
+    (void)fullWordCreated;
+    (void)index;
+    (void)r;
+    (void)c;
+    (void)stuffBefore;
+    (void)stuffAfter;
+    (void)spaceInTiles;
     //add any char before tiles to the word
     r = (direction == 'V') ? row-1 : row;
     c = (direction == 'H') ? col-1 : col;
@@ -159,9 +179,16 @@ bool checkValid(GameState *game, int row, int col, char direction, const char *t
     return false;
 }
 
-GameState* place_tiles(GameState *game, int row, int col, char direction, const char *tiles, int *num_tiles_placed) {
+GameState *place_tiles(GameState *game, int row, int col, char direction, const char *tiles, int *num_tiles_placed) {
+    (void)game;
+    (void)row;
+    (void)col;
+    (void)direction;
+    (void)tiles;
+    *num_tiles_placed = 0;
+
     int tilesPlaced = 0;
-    if(!checkValid(game, row, col, direction, tiles)) {
+    if (!checkValid(game, row, col, direction, tiles)) {
         *num_tiles_placed = tilesPlaced;
         return game;
     }
@@ -170,11 +197,11 @@ GameState* place_tiles(GameState *game, int row, int col, char direction, const 
     int **boardDepth = game->boardDepth;
 
     struct GameState *newGame = (struct GameState*)malloc(sizeof(struct GameState));
-    
-    int tilesLen = 0;
-    while(*tiles != '\0') {
-        tilesLen++;
-    }
+
+    (void)newGame;
+    (void)board;
+    (void)boardDepth;
+    int tilesLen = strlen(tiles);
 
     if(direction == 'H' && col + tilesLen > game->numOfCol) {//check if board needs to be expanded
         newGame->numOfCol = col + tilesLen;
@@ -186,7 +213,7 @@ GameState* place_tiles(GameState *game, int row, int col, char direction, const 
         newGame->numOfCol = col;
         newGame->numOfRow = row;
     }
-    
+
     char **newBoard = (char **)malloc(newGame->numOfRow * sizeof(char *));
     for(int i = 0; i < newGame->numOfRow; i++) {
         newBoard[i] = (char *)malloc(newGame->numOfCol * sizeof(char));
@@ -218,7 +245,7 @@ GameState* place_tiles(GameState *game, int row, int col, char direction, const 
             }
         }
     }
-    
+
     newGame->board = newBoard;
     newGame->boardDepth = newBoardDepth;
     newGame->previous = game;
@@ -227,7 +254,7 @@ GameState* place_tiles(GameState *game, int row, int col, char direction, const 
     return newGame;
 }
 
-GameState* undo_place_tiles(GameState *game) {
+GameState *undo_place_tiles(GameState *game) {
     (void)game;
     return NULL;
 }
